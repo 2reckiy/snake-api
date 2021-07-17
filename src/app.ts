b96ceedd4c5ebf35cf89b7762316b991091c6461
Max Ivankovitser <ivankovitser.max@gmail.com>
2021-07-17 16:37:38 +0300

    update cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: false,
   }
 });
 const port = 3000;
